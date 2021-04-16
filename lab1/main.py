2828c9cc8aae3b6e4f42c2006d3be55321933226
avion123678 <avion123678@gmail.com>
2021-04-16 14:04:54 +0300

    Update main.py

--- lab1/main.py
+++ lab1/main.py
@@ -2,8 +2,8 @@ from brents_method import brents_method
 from parabolic_interpolation import parabolic_interpolation
 from half_interval import half_divide_method
 from golden_ratio import golden_ratio
-import math as m
 from fibonacci_method import fibonacci_searcher
+import math as m
 
 
 def f(x):
@@ -14,9 +14,10 @@ if __name__ == '__main__':
     a = -5.0
     b = 2.5
     e = 0.005
+    eps = 0.01
     print(half_divide_method(a=a, b=b, e=e, f=f))
     print(golden_ratio(l=a, r=b, eps=e, f=f))
     print(parabolic_interpolation(x1=a, x3=b, e=e, function=f))
     print(brents_method(a=a, c=b, e=e, function=f))
-    searcher = fibonacci_searcher(lambda x: eval("m.exp(m.sin(x)) * x ** 2"))
-    print(searcher.find_local_min(a, b, e, 0.01))
+    searcher = fibonacci_searcher(f)
+    print(searcher.find_local_min(a, b, e, eps))
