a41c38b9ce7dec9398a476cfb86f3e2494c1a73c
Mark <bigfirestart@gmail.com>
2021-04-16 13:34:05 +0300

    fix: half interval

--- lab1/half_interval.py
+++ lab1/half_interval.py
@@ -2,10 +2,13 @@ import math
 
 
 def half_divide_method(a: float, b: float, e: float, f: ()):
-    iter_count = 0
-    x = (a + b) / 2
-    while math.fabs(f(x)) >= e:
-        x = (a + b) / 2
-        a, b = (a, x) if f(a) * f(x) < 0 else (x, b)
-    return (a + b) / 2
+    c = (a+b)/2
+    while abs(b-a) > e:
+        c = (a + b) / 2
+        if f(a)-f(c) < 0:
+            b = c
+        else:
+            a = c
+    return c
+
 
